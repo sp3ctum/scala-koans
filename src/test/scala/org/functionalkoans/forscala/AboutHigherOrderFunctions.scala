82a74df5f6aba3ec9d97443af2f54509322d52e9
Mika Vilpas <mika.vilpas@gmail.com>
2016-08-22 11:51:03 +0300

    Solve AboutHigherOrderFunctions

--- src/test/scala/org/functionalkoans/forscala/AboutHigherOrderFunctions.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutHigherOrderFunctions.scala
@@ -1,5 +1,6 @@
 package org.functionalkoans.forscala
 
+import scala.collection.immutable.Range
 import support.KoanSuite
 
 class AboutHigherOrderFunctions extends KoanSuite {
@@ -24,18 +25,18 @@ class AboutHigherOrderFunctions extends KoanSuite {
     val result4 = lambda4(3)
     val result5 = lambda5(3)
 
-    result should be(__)
-    result1andhalf should be(__)
-    result2 should be(__)
-    result3 should be(__)
-    result4 should be(__)
-    result5 should be(__)
+    result should be(4)
+    result1andhalf should be(4)
+    result2 should be(4)
+    result3 should be(4)
+    result4 should be(4)
+    result5 should be(4)
   }
 
   koan("An anonymous function can also take on a different look by taking out the brackets") {
     def lambda = (x: Int) => x + 1
     def result = lambda(5)
-    result should be(__)
+    result should be(6)
   }
 
   koan("Meet closure. Closure is any function that closes over the environment") {
@@ -46,12 +47,12 @@ class AboutHigherOrderFunctions extends KoanSuite {
     }
 
     val result1 = closure(10)
-    result1 should be(__)
+    result1 should be(11)
 
     incrementer = 2
 
     val result2 = closure(10)
-    result2 should be(__)
+    result2 should be(12)
   }
 
   koan("We can take that closure and throw into a method and it will still hold the environment") {
@@ -62,37 +63,38 @@ class AboutHigherOrderFunctions extends KoanSuite {
     def closure = (x: Int) => x + incrementer
 
     val result = summation(10, closure)
-    result should be(__)
+    result should be(13)
 
     incrementer = 4
     val result2 = summation(10, closure)
-    result2 should be(__)
+    result2 should be(14)
   }
 
   koan("function returning another function") {
     def addWithoutSyntaxSugar(x: Int) = {
-      new Function1[Int, Int]() {
-        def apply(y: Int): Int = x + y
-      }
+      // new Function1[Int, Int]() {
+      //   def apply(y: Int): Int = x + y
+      // }
+      y:Int => y + x
     }
     addWithoutSyntaxSugar(1).
-      isInstanceOf[Function1[Int,Int]] should be(__)
+      isInstanceOf[Function1[Int,Int]] should be(true)
 
-    addWithoutSyntaxSugar(2)(3) should be(__)
+    addWithoutSyntaxSugar(2)(3) should be(5)
 
     def fiveAdder = addWithoutSyntaxSugar(5)
-    fiveAdder(5) should be(__)
+    fiveAdder(5) should be(10)
   }
 
   koan("function returning another function " +
     "using an anonymous function") {
     def addWithSyntaxSugar(x: Int) = (y:Int) => x + y
 
-    addWithSyntaxSugar(1).isInstanceOf[Function1[Int,Int]] should be(__)
-    addWithSyntaxSugar(2)(3) should be(__)
+    addWithSyntaxSugar(1).isInstanceOf[Function1[Int,Int]] should be(true)
+    addWithSyntaxSugar(2)(3) should be(5)
 
     def fiveAdder = addWithSyntaxSugar(5)
-    fiveAdder(5) should be(__)
+    fiveAdder(5) should be(10)
   }
 
 
@@ -102,7 +104,7 @@ class AboutHigherOrderFunctions extends KoanSuite {
       | at runtime.""") {
     def addWithSyntaxSugar(x: Int) = (y:Int) => x + y
 
-    addWithSyntaxSugar(1).isInstanceOf[Function1[Int, Int]] should be(__)
+    addWithSyntaxSugar(1).isInstanceOf[Function1[Int, Int]] should be(true)
   }
 
 
@@ -111,28 +113,28 @@ class AboutHigherOrderFunctions extends KoanSuite {
     """function taking another function as parameter. Helps in composing functions.
       | Hint: a map method applies the function to each element of a list""") {
 
-    def makeUpper(xs: List[String]) = xs map {_.toUpperCase}
+    def makeUpper(xs: List[String]) = xs map {_.toLowerCase}
 
     def makeWhatEverYouLike(xs: List[String], sideEffect: String => String) = {
       xs map sideEffect
     }
 
-    makeUpper(List("abc", "xyz", "123")) should be(__)
+    makeUpper(List("abc", "xyz", "123")) should be(List("abc", "xyz", "123"))
 
     makeWhatEverYouLike(List("ABC", "XYZ", "123"), {
       x => x.toLowerCase
-    }) should be(__)
+    }) should be(List("abc", "xyz", "123"))
 
     //using it inline
-    List("Scala", "Erlang", "Clojure") map {_.length} should be(__)
+    List("Scala", "Erlang", "Clojure") map {_.length} should be(List(5, 6, 7))
   }
 
   koan("Currying is a technique to transform function with multiple parameters to function with one parameter") {
     def multiply(x: Int, y: Int) = x * y
-    (multiply _).isInstanceOf[Function2[_, _, _]] should be(__)
+    (multiply _).isInstanceOf[Function2[_, _, _]] should be(true)
     val multiplyCurried = (multiply _).curried
-    multiply(4, 5) should be(__)
-    multiplyCurried(3)(2) should be(__)
+    multiply(4, 5) should be(20)
+    multiplyCurried(3)(2) should be(6)
   }
 
   koan("Currying allows you to create specialized version of generalized function") {
@@ -141,10 +143,10 @@ class AboutHigherOrderFunctions extends KoanSuite {
     }
     def onlyEven(x: Int) = x % 2 == 0
     val xs = List(12, 11, 5, 20, 3, 13, 2)
-    customFilter(onlyEven)(xs) should be(__)
+    customFilter(onlyEven)(xs) should be(List(12, 20, 2))
 
     val onlyEvenFilter = customFilter(onlyEven) _
-    onlyEvenFilter(xs) should be(__)
+    onlyEvenFilter(xs) should be(List(12, 20, 2))
 
   }
 }
