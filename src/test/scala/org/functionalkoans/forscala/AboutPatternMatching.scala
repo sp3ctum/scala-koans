ddacacadb2f057e2d908ed83db498efc6160e593
Mika Vilpas <mika.vilpas@gmail.com>
2016-08-22 16:36:35 +0300

    Solve AboutPatternMatching

--- src/test/scala/org/functionalkoans/forscala/AboutPatternMatching.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutPatternMatching.scala
@@ -16,8 +16,7 @@ class AboutPatternMatching extends KoanSuite {
       case _ => println(stuff); 0 //case _ will trigger if all other cases fail.
     }
 
-    myStuff should be(__)
-
+    myStuff should be(2)
   }
 
   koan("Pattern matching can return complex somethings") {
@@ -30,13 +29,10 @@ class AboutPatternMatching extends KoanSuite {
       case _ => println(stuff); 0
     }
 
-    myStuff should be(__, __, __)
-
+    myStuff should be((0, 0, 255))
   }
 
   koan("Pattern matching can match complex expressions") {
-
-
     def goldilocks(expr: Any) = expr match {
       case ("porridge", "Papa") => "Papa eating porridge"
       case ("porridge", "Mama") => "Mama eating porridge"
@@ -44,7 +40,7 @@ class AboutPatternMatching extends KoanSuite {
       case _ => "what?"
     }
 
-    goldilocks(("porridge", "Mama")) should be(__)
+    goldilocks(("porridge", "Mama")) should be("Mama eating porridge")
 
   }
 
@@ -57,8 +53,8 @@ class AboutPatternMatching extends KoanSuite {
       case _ => "what?"
     }
 
-    goldilocks(("porridge", "Papa")) should be(__)
-    goldilocks(("chair", "Mama")) should be(__)
+    goldilocks(("porridge", "Papa")) should be("eating")
+    goldilocks(("chair", "Mama")) should be("sitting")
 
   }
 
@@ -71,10 +67,9 @@ class AboutPatternMatching extends KoanSuite {
       case _ => "what?"
     }
 
-    goldilocks(("porridge", "Papa")) should be(__)
-    goldilocks(("chair", "Mama")) should be(__)
+    goldilocks(("porridge", "Papa")) should be("Papa said someone's been eating my porridge")
+    goldilocks(("chair", "Mama")) should be("Mama said someone's been sitting in my chair")
   }
-
 
   koan("Pattern matching can done on regular expression groups") {
     val EatingRegularExpression = """Eating Alert: bear=([^,]+),\s+source=(.+)""".r //.r turns a String to a regular expression
@@ -88,8 +83,8 @@ class AboutPatternMatching extends KoanSuite {
       case _ => "what?"
     }
 
-    goldilocks("Eating Alert: bear=Papa, source=porridge") should be(__)
-    goldilocks("Sitting Alert: bear=Mama, source=chair") should be(__)
+    goldilocks("Eating Alert: bear=Papa, source=porridge") should be("Papa said someone's been eating my porridge")
+    goldilocks("Sitting Alert: bear=Mama, source=chair") should be("Mama said someone's been sitting on my chair")
   }
 
   koan( """A backquote can be used to refer to a stable variable in scope to create a case statement.
@@ -103,10 +98,10 @@ class AboutPatternMatching extends KoanSuite {
       case _ => "what?"
     }
 
-    goldilocks(("porridge", "Papa")) should be(__)
-    goldilocks(("chair", "Mama")) should be(__)
-    goldilocks(("porridge", "Cousin")) should be(__)
-    goldilocks(("beer", "Cousin")) should be(__)
+    goldilocks(("porridge", "Papa")) should be("eating")
+    goldilocks(("chair", "Mama")) should be("sitting")
+    goldilocks(("porridge", "Cousin")) should be("eating")
+    goldilocks(("beer", "Cousin")) should be("what?")
   }
 
   koan("A backquote can be used to refer to a method parameter as a stable variable to create a case statement.") {
@@ -115,9 +110,9 @@ class AboutPatternMatching extends KoanSuite {
       case `i` => true
       case _ => false
     }
-    patternEquals(3, 3) should be(__)
-    patternEquals(7, 9) should be(__)
-    patternEquals(9, 9) should be(__)
+    patternEquals(3, 3) should be(true)
+    patternEquals(7, 9) should be(false)
+    patternEquals(9, 9) should be(true)
   }
 
   koan(
@@ -129,7 +124,7 @@ class AboutPatternMatching extends KoanSuite {
       case _ => 0
     }
 
-    secondElement should be(__)
+    secondElement should be(2)
   }
 
   koan(
@@ -140,7 +135,7 @@ class AboutPatternMatching extends KoanSuite {
       case _ => 0
     }
 
-    secondElement should be(__)
+    secondElement should be(List(3))
   }
 
   koan(
@@ -150,7 +145,7 @@ class AboutPatternMatching extends KoanSuite {
       case _ => 0
     }
 
-    secondElement should be(__)
+    secondElement should be(0)
   }
 
   koan(
@@ -162,7 +157,7 @@ class AboutPatternMatching extends KoanSuite {
       case _ => 0
     }
 
-    r should be(__)
+    r should be(0)
   }
 }
 
